25e82d13b6af4b4cf0e06bd1b68182e57fb2f67f
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-14 15:41:41 +0200

    created all the scaffolding project, added new class for products parent

---
+++ classes/products.php
@@ -0,0 +1,21 @@
+<?php
+    //Parent
+
+    class Product {
+        protected $title;
+        protected $subtitle;
+        protected $description;
+        protected $price;
+
+        public function __construct($title, $subtitle, $description, $price) {
+            $this->title = $title;
+            $this->subtitle = $subtitle;
+            $this->description = $description;
+            $this->price = $price;
+        }
+
+        // Add getter methods for the properties
+
+        // Add any additional methods you need
+    }
+?>
