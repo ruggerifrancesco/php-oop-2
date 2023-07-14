71c18ae901cab82dc4be5d3e8823bfeb5659f2f7
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-14 16:03:47 +0200

    constrcuted raw class for food and toy products

--- classes/products/food.php
+++ classes/products/food.php
@@ -2,7 +2,32 @@
     include_once __DIR__ . '../../products.php';
 
     class Food extends Products {
-        // Add specific properties and methods for food products
+        private $nutritionalValues;
+        private $foodType;
+    
+        public function __construct(
+                $title, 
+                $subtitle, 
+                $description, 
+                $price, 
+                $nutritionalValues, 
+                $foodType,
+            ) {
+            parent::__construct($title, $subtitle, $description, $price);
+            $this->nutritionalValues = $nutritionalValues;
+            $this->foodType = $foodType;
+        }
+    
+        // Add getter methods for the new properties
+        public function getNutritionalValues() {
+            return $this->nutritionalValues;
+        }
+    
+        public function getFoodType() {
+            return $this->foodType;
+        }
+    
+        // Add any additional methods specific to food products
     }
-
-?>
+    
+    ?>
--- classes/products/toy.php
+++ classes/products/toy.php
@@ -2,7 +2,25 @@
     include_once __DIR__ . '../../products.php';
 
     class Toy extends Products {
-        // Add specific properties and methods for toy products
+        private $nutritionalValues;
+        private $foodType;
+    
+        public function __construct($title, $subtitle, $description, $price, $nutritionalValues, $foodType) {
+            parent::__construct($title, $subtitle, $description, $price);
+            $this->nutritionalValues = $nutritionalValues;
+            $this->foodType = $foodType;
+        }
+    
+        // Add getter methods for the new properties
+        public function getNutritionalValues() {
+            return $this->nutritionalValues;
+        }
+    
+        public function getFoodType() {
+            return $this->foodType;
+        }
+    
+        // Add any additional methods specific to food products
     }
 
 ?>
