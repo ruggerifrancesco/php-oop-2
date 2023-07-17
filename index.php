5507d7d4f5cfee80493590e8d7fc975069beebb5
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-17 05:22:14 +0200

    created strcuture of card with boostrap component

--- index.php
+++ index.php
@@ -5,10 +5,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>E-commerce</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Dogs</h1>
-    <h1>Cats</h1>
+
+    <div class="container">
+        <h1 class="text-center">Products List</h1>
+
+        <div class="row">
+            
+        <div class="col">
+            <div class="card border-info mb-3">
+                <div class="card-header">Header</div>
+                    <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+            </div>
+        </div>
+
+
+        </div>
+    </div>
 </body>
 </html>
